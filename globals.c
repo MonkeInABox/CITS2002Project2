1e1b0c59f09d6a9c01767870168a470d45dd04cd
Skoll-8 <dylanarto@live.com.au>
2023-10-02 11:04:45 +0800

    n

---
+++ globals.c
@@ -0,0 +1,5 @@
+#include "mysync.h"
+
+//GLOBALS INTITIALISED
+char *dirNames[/* no. diretories */];
+char *dirModified[/* no. files? or no. directories? */];        //match up dirModified indexes w/ dirNames indexes?
--- mysync.h
+++ mysync.h
@@ -1,1 +1,12 @@
-//GLOBAL VARIABLES
+#include  <stdio.h>
+#include  <stdbool.h>
+
+//GLOBAL PROCESSOR CONSTANTS
+
+
+//GLOBAL FUNCTIONS
+extern void mysync();
+
+//GLOBAL VARIABLES DECLARED
+extern char *dirNames[];
+extern char *dirModified[];
