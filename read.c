697b337fab7c02b6162bd5f670625f085f4e5551
MonkeInABox <jezb03@outlook.com>
2023-10-09 10:55:42 +0800

    ermmmmwhuh

--- read.c
+++ read.c
@@ -10,14 +10,13 @@ void readFile(char fileName[]){
         }
     }
     fclose(compFile);
-    return fileCopy1;
 }
 
 void readDir(char dirName[]){
     struct dirent* dirEntry;
     DIR* dir = opendir(dirName);
     int i = 0;
-    while((de = readdir(dir)) != NULL){
+    while((dirEntry = readdir(dir)) != NULL){
         strcpy(dirCopy1[i], dirEntry->d_name);
         i++;
     }
