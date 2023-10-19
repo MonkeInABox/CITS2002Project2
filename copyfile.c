bbbf56ea99e29810ef5e058ac1e476671c9d1b61
MonkeInABox <jezb03@outlook.com>
2023-10-19 15:21:30 +0800

    quick my guy ots ball sucking hour

--- copyfile.c
+++ copyfile.c
@@ -6,6 +6,9 @@ void copyfile(struct stat *stats1, char *name1, struct stat stats2, char *name2)
         FILE *fp;
         fp  = fopen (name1, "w");              //write ONLY BITCHES
         fclose(fp);*/
-        
+        struct utimbuf new_times;
+        new_times.actime = stats1->st_atime; /* keep atime unchanged */
+        new_times.modtime = time(NULL);    /* set mtime to current time */
+        utime(name1, &new_times);
     }
 }
