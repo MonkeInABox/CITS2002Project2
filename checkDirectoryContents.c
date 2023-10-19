a4fd60946b5397292175e30711e1f6d320a45fff
Skoll-8 <dylanarto@live.com.au>
2023-10-19 21:18:59 +0800

    Update checkDirectoryContents.c

--- checkDirectoryContents.c
+++ checkDirectoryContents.c
@@ -1,8 +1,11 @@
 #include mysync.h
 void checkDirectoryContents(DIR* dir, char *currentDirName){
     struct dirent* dirEntry;
-    for(int i; i < 100; i++){ //the i < 100 should be replaced with the size of the dirNames array.
-        if(strcmp(directoryName, dirNames[i]) != 0){
+    for(int i; i < 100; i++){
+        if(strcmp(dirNames[i], "0") == 0){
+            break;
+        }
+        if(strcmp(currentDirName, dirNames[i]) != 0){
             while((dirEntry = readdir(dir)) != NULL){
                 if(strcmp(newDir[i], dirEntry->d_name) = 0){
                     copyfile();
