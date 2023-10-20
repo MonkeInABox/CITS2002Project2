4caa5350b777b098d29f377b012627293c5bd02c
MonkeInABox <jezb03@outlook.com>
2023-10-20 09:36:29 +0800

    sdfgh

--- checkDirectoryContents.c
+++ checkDirectoryContents.c
@@ -4,15 +4,20 @@ void checkDirectoryContents(){
     int copy = 0;
     for(int i = startIndex[0]; i < startIndex[1]; i++){
         makingNewFile(0, i);
+        printf("HERE5\n");
     }
-    for(int i = 0; i < startIndex[0]; i++){
+    printf("HERE2\n");
+    for(int i = 0; i < startIndex[1]; i++){
         for(int j = startIndex[0]; j < startIndex[1]; j++){
+            printf("HERE1\n");
             if(strcmp(newDir[i], newDir[j]) == 0){
                 copy = 1;
+                printf("HERE3\n");
                 break;
             }
         }
         if(copy == 0){
+            printf("HERE4\n");
             makingNewFile(startIndex[0], i);
         }
     }   
