a73bf68e83b95add47ab14e68b8b536c22a5d1a9
MonkeInABox <jezb03@outlook.com>
2023-10-20 08:58:01 +0800

    rtyui

--- checkDirectoryContents.c
+++ checkDirectoryContents.c
@@ -3,13 +3,13 @@
 void checkDirectoryContents(){
     int copy = 0;
     for(int i = startIndex; i < 100; i++){
-        if(strcmp(newDir[i], "0") = 0){
+        if(strcmp(newDir[i], "0") != 0){
             break;
         }
         makingNewFile(0, i);
     }
     for(int i = 0; i < startIndex; i++){
-        for(int j = startIndex; j < 100, j++){
+        for(int j = startIndex; j < 100; j++){
             if(strcmp(newDir[i], newDir[j]) == 0){
                 copy = 1;
                 break;
